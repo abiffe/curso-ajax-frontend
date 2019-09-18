78046a5121558b4325876ce3d1937223dbe260a8
André <andre.renzo@ifpr.edu.br>
2019-09-18 20:20:41 -0300

    Aula 3

--- js/main.js
+++ js/main.js
@@ -1,59 +1,118 @@
 var urlAtual = $(location);
 $(document).ready(function () {
-    /*console.log($("h1").text());
-    var bemVindo = $(".bem-vindo");
-    bemVindo.text("Olá Seja Bem Vindo!");
-    bemVindo.removeClass("bem-vindo");
-    var h2 = $("h2");
-    h2.text("Estou aqui para aprender");
-    h2.addClass("bem-vindo-red");
-    $(".navbar-brand").text("Sistema"); 
-    console.log($("ul li"));
-    console.log($("ul > li"));
-    console.log($("ul.navbar-nav li"));
-    console.log($("ul.navbar-nav li.nav-item"));
-    console.log($("li a"));
-    console.log($("li a[href!='#']"));*/
-    $("ul.navbar-nav li.nav-item").each(function () {
-       let $this = $(this);
-        console.log($this);
-        if($this.hasClass("active")){
-            console.log("Ativo");
-            $this.removeClass("active");
-        }
-       console.log(urlAtual.attr("href").includes("form.html"));
-       
-       if(urlAtual.attr("href").includes("form.html")){
-        //console.log("Form");   
-        //console.log($this.find("a").attr("href") == "form.html");
-        let href = $this.find("a").attr("href");
-        if(href == "form.html"){
-            $this.addClass("active");
-        }   
-        //$("li > a[href='form.html']").addClass("active");
-       } 
-
-       if(urlAtual.attr("href").includes("index.html")){
-            console.log($("li > a[href='index.html']"));
-            $("li > a[href='index.html']").addClass("active");
-        } 
-    });
-    /*$("h1").click(function () {
-        $(this).toggleClass("bem-vindo"); 
-    });*/
-    $("h1").on("click mouseenter", function () {
-        $(this).toggleClass("bem-vindo"); 
-    });
-
     quantidadeCaracteres();
-
+    paginaAtual();
+    apareceAjuda();
+    mostraSenha();
+    $("#ajuda").animate({
+        opacity: 0.3,
+        "margin-left": "10em",
+    }, {
+        duration: 3000,
+        easing: "linear"
+    });
+    $("#ajuda2").animate({
+        opacity: 0.3,
+        "margin-left": "10em",
+    }, {
+        duration: 3000,
+        easing: "swing"
+    })
 });
 
 /**
  * função e evento de keypress para contar os caracteres de um textarea
  */
 function quantidadeCaracteres() {
-    $("#campoDigitacao").on("keypress", function () {
-        console.log($(this).val().length);
+    $("#campoDigitacao").on("keypress input", function () {
+        let campoDigitacao = $(this);
+        campoDigitacao.off("keypress");
+        console.log(campoDigitacao.val().length);
+        let caracteresInformados = $("#caracteres_informados");
+        if(caracteresInformados.length == 0){
+            campoDigitacao.parent().append("<p id='caracteres_informados'>"+
+            "Caracteres Digitados "+campoDigitacao.val().length+"</p>");
+            removeContador();
+        }else{
+            caracteresInformados.text("Caracteres Digitados "+campoDigitacao.val().length);
+        }
+    });
+}
+
+/**
+ * função verificar em qual página estamos e mostrar na navbar
+ */
+function paginaAtual() {
+    /**
+     * obtenha todos li com classe nav-item que sejam filhos
+     * de um ul com classe navbar-nav
+     */
+    $("ul.navbar-nav li.nav-item").each(function () {
+        let $this = $(this);
+        /**
+         * separando as funções executadas para compreender:
+         * $this.find("a").attr("href") -->  
+         *  a partir do elemento atual (<li>) encontre um elemento <a> e pegue seu atributo href
+         * urlAtual.attr("href").includes("string a ser procurada") --> 
+         *  verifica se o atributo da urlAtual contém a string que estamos procurando
+         */
+         if(urlAtual.attr("href").includes($this.find("a").attr("href"))){
+             /**
+              * caso encontre, ou seja, caso a página atual seja 
+              * o elemento que estamos lendo, marque como ativo
+              */
+             $this.addClass("active");
+         }else{
+             //se não, se tiver uma classe ativo, remova ela
+             if($this.hasClass("active")){
+                 $this.removeClass("active");
+             }
+         }
+     });
+}
+
+/**
+ * função para mostrar ou retirar o texto de ajuda
+ */
+function apareceAjuda() {
+    $("#ajuda").on("click", function () {
+         let textoAjuda = $("#textoAjuda");
+         if(textoAjuda.is(":hidden")){
+            textoAjuda.stop().slideDown("slow");
+         }else{
+            textoAjuda.stop().slideUp("slow");
+         }
+    });
+}
+
+/**
+ * remover o contador de caracteres quando houver um double click
+ */
+function removeContador() {
+    $("#caracteres_informados").one("dblclick", function () {
+        $(this).remove();
+    });
+}
+ /**
+  * função para mostrar a senha
+  */
+function mostraSenha() {
+    let password = $("#password");
+    password.on("mouseenter mouseleave", function (e) {
+        console.log(e);
+        if($(e).attr("type") == "mouseenter"){
+            password.attr("type", "text");
+        }
+        if($(e).attr("type") == "mouseleave"){
+            password.attr("type", "password");
+        }
+        /*if(password.attr("type") == "password"){
+            password.attr("type", "text");
+        }else{
+            password.attr("type", "password");
+        }*/
+        /*setInterval(function () {
+            password.attr("type", "password");
+        }, 2000);*/
     });
 }
